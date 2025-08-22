55aaf1df755a7a08d8447a7b3bc1120fa2923481
iSeeQu <seequ@gmx.ch>
2025-08-22 20:27:41 +0200

    Update rules.mk

--- firmware/rules.mk
+++ firmware/rules.mk
@@ -1,34 +1,28 @@
-# rules.mk - Definiert die Build-Regeln für das Keyboard.
-# Hier legen wir fest, welche Funktionen kompiliert werden sollen.
-
-# Name des Mikrocontrollers, wie in QMK bekannt.
 MCU = STM32F072
-
-# Compiler-Einstellungen
-# Aktiviert die Optimierung für kleinere Firmware-Größen.
-OPT_DEFS += -Os -I.
-
-# QMK-Funktionen, die aktiviert werden sollen
-# Aktiviert die Funktionalität für eine einfache Tastatur.
-SRC += matrix.c
-SRC += debounce.c
-SRC += raw_hid.c
-
-# KEYMAP_C_SRC wird benötigt, um die keymap.c Datei zu kompilieren
-KEYMAP_C_SRC = keymap.c
-
-# Aktiviert das BOOTLOADER-Flashing.
-# Wird benötigt, um die Firmware auf den Controller zu laden.
 BOOTLOADER = stm32-dfu
+SERIAL_DRIVER = vendor
 
-# USB-Konfiguration
-# Wird für die Kommunikation mit dem Host-Computer benötigt.
-# VCOM wird benötigt, um serielle Daten an den Host zu senden (hilfreich fürs Debugging)
-# Und HID muss auch aktiviert werden.
-SRC += usb_cdc.c
-SRC += usb_hid.c
+# QMK Features
+LTO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
+MOUSEKEY_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
 
-# Um QMK zu sagen, dass der VCOM Port auf den COM Port der Tastatur gemapped werden soll,
-# müssen wir diesen Flag hinzufügen.
-SRC += usb_serial_vcp.c
+# Enable RGB Lighting (falls benötigt)
+RGBLIGHT_ENABLE = no
+RGB_MATRIX_ENABLE = no
 
+# Optimierungen
+OPT_DEFS += -flto
+
+# Firmware-Optionen
+BOOTMAGIC_ENABLE = no       # Bootmagic deaktivieren
+MOUSEKEY_ENABLE = yes        # Maussteuerung aktivieren
+EXTRAKEY_ENABLE = yes        # Mediensteuerung aktivieren
+CONSOLE_ENABLE = no          # Konsolenausgabe deaktivieren
+COMMAND_ENABLE = no          # Befehlsfunktionen deaktivieren
+NKRO_ENABLE = yes            # N-Key Rollover aktivieren
+RGBLIGHT_ENABLE = no         # RGB-Beleuchtung deaktivieren
+UNICODE_ENABLE = no          # Unicode deaktivieren
